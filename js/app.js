12ad9d3e41fea9a795fa86412c90b1f144b1bd04
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-02-11 15:44:11 -0600

    set some variables and cached elements

--- js/app.js
+++ js/app.js
@@ -7,10 +7,18 @@ console.log("Hello world")
 
 /*-------------------------------- Variables --------------------------------*/
 
-
+let totalMarbles = 24;
+let currentPlayer;
+let board= []
+let player1All= totalMarbles
+let player2All= totalMarbles
 
 /*------------------------ Cached Element References ------------------------*/
-
+const p1BankEl = document.querySelector('#bank1')
+const p2BankEl = document.querySelector('#bank2')
+const howToBtn = document.querySelector('how-to')
+const resetBtn = document.querySelector('reset')
+const holeEls = document.querySelectorAll('.hole')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
