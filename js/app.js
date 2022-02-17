adbb511638be6fe5007bda15d022e76de07b91c2
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-02-16 19:50:47 -0600

    clean and revise js blocks

--- js/app.js
+++ js/app.js
@@ -1,19 +1,9 @@
-
-
-
 /*-------------------------------- Constants --------------------------------*/
-
 const chimes = new Audio('./audio/chimes.wav')
 
 /*-------------------------------- Variables --------------------------------*/
-
-
-
-let win, turn, winner, p1Manc, p2Manc, marbles, numMarbles, currentIndex
+let win, turn, winner, first, marbles, numMarbles, currentIndex, p1Manc, p2Manc
 let board= []
-
-
-
 
 /*------------------------ Cached Element References ------------------------*/
 const holeElements = document.querySelectorAll('.hole')
@@ -21,10 +11,7 @@ const p1MancEl = document.querySelector('#hole6')
 const p2MancEl = document.querySelector('#hole13')
 const howToBtn = document.getElementById("howToButton")
 const resetBtn = document.getElementById('reset')
-
-
-
-const color1= document.getElementById('player1')
+const color1 = document.getElementById('player1')
 const color2 = document.getElementById('player2')
 
 
@@ -41,24 +28,19 @@ holeElements.forEach(function(hole) {
 //p1MancEl.addEventListener('mouseover', handleHover)
 //p2MancEl.addEventListener('mouseover', handleHover)
 
-howToBtn.addEventListener('click', getList) 
-resetBtn.addEventListener('click', init) //can happen anytime, not just when game complete
+resetBtn.addEventListener('click', init) 
 
 
 /*-------------------------------- Functions --------------------------------*/
 init();
 
-
-
 function init() {
   board = [4,4,4,4,4,4,0,4,4,4,4,4,4,0]
-  
-  p1Manc=board[6]
-  p2Manc=board[13]
+  p1Manc = board[6]
+  p2Manc = board[13]
   turn = 1
   winner = null 
   render()
-  
 }
 
 function render(){ 
@@ -66,90 +48,74 @@ function render(){
     let holeElementIndex = hole.id.slice(4)
     hole.textContent = board[holeElementIndex]
   })
-  if (turn===1) {
+  if (turn === 1) {
     color1.style.color="rgb(100, 13, 21)"
-  }else {
+  } else {
     color1.style.color="black"
   }
-  if (turn===2) {
+  if (turn === 2) {
     color2.style.color="#FFB94F"
-  }else {
+  } else {
     color2.style.color="black"
   }
 }
 
-function handleClick(evt){ //distributes marbles around the board from player1 perspective
+function handleClick(evt){ 
   let first = parseInt(evt.target.id.slice(4))
-  //setting up first play conditions to limit players to certain indexes
-  if (board[first]===0){
+  
+  if (board[first] === 0){
     return 
   }
-  if (first === 6 || first === 13){
-    //console.log('nononono')
+  if (first === 6 || first === 13) {
     return
   }
   if (first > 5 && turn === 1) {
-    //console.log('nono')
     return
   }
   if (first < 6 && turn === 2) {
-    //console.log('no')
     return
   }
   
   marbles = board[first]
   board[first] = 0
+  
   for (let i = 1; i <= marbles; i++) {
     chimes.play()
     numMarbles = board[first+i]
-    //console.log(marbles)
     if (turn === 1 && first+i === 13) {
       first++
     }
-    if (turn === 2 && first+i === 6){
+    if (turn === 2 && first+i === 6) {
       first++
     }
     if ((first + i) > 13) {
       first = 0 - i 
-      }
+    }
     board[first+i]++
-      //console.log(numMarbles)
-    currentIndex = first+i //what each index a marble is  distributed to
-    //console.log(currentIndex)
+    currentIndex = first+i 
   }
-  if (numMarbles===0){
-    absorb();
+  if (numMarbles === 0) {
+    absorb()
   }
-  
   goAgain();
-  
   switchTurn();
   render();
   getWinner();
 }
 
-function switchTurn(){
-  if (turn === 1){
+function switchTurn() {
+  if (turn === 1) {
     turn++
-  }else {
+  } else {
     turn--
   }
-  
 }
 
-
-  
-  
-
-    
-  
-
-
-function goAgain(){
+function goAgain() {
   if (turn === 1 && currentIndex === 6) {
     turn++
   }
-  if (turn === 2 && currentIndex === 13){
+  if (turn === 2 && currentIndex === 13) {
     turn++
   }
 }
@@ -214,48 +180,14 @@ function absorb(){
   }
 }
 
-// function checkIfOver(){
-//   if (player1Over() || player2Over()){
-//     getWinner();
-//   }
-// }
-
-// function player1Over(){
-//   for (let i = 0; i < board.length; i++){
-//     if (board[i] === 0 && board[i] < 6){
-//     return false
-//     }
-//   }
-//   return true;
-// }
-
-// function player2Over(){
-//   for (let i = 0; i < board.length; i++){
-//     if (board[i] === 7 && board[i] < 13){
-//     return false
-//     }
-//   }
-//   return true;
-// }
-
-
-
-
-
-
-function getWinner(){
+function getWinner() {
   let rowP1 = board.slice(0,6).reduce(function(a, b){
     return a + b
   }, 0)
-    
-  
-    let rowP2 = board.slice(7,13).reduce(function(a, b){
+  let rowP2 = board.slice(7,13).reduce(function(a, b){
     return a + b
   }, 0)
-
-
-
-  if (rowP1 === 0 || rowP2 === 0) {
+if (rowP1 === 0 || rowP2 === 0) {
     rowP1 += board[6]
     rowP2 += board[13]
     
@@ -271,9 +203,9 @@ function getWinner(){
       alert(`How in the world did you tie?`)
       return
     }
-    
     alert(`How exciting! The winner is Player ${winner}!`)
-  }}
+  }
+}
 
 
 
@@ -283,7 +215,4 @@ function getWinner(){
 
 
 
-function getList(){
-console.log("Instructions") //this works
-}
 
